bcd41d91942f2ab4952dceb0948d083889d0967b
igor.pieruccini <igorpieruccini@gmail.com>
2021-08-11 19:00:51 +0200

    feat (login)
    add a simple loggin design for the page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,12 +3,34 @@ import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
 
+  void onPressed () {
+    // auth && and redirect page
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(child: Text("Login Page")),
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Welcome", style: Theme.of(context).textTheme.headline4 ),
+              TextFormField(decoration: const InputDecoration(
+                icon: Icon(Icons.account_circle_rounded),
+                labelText: "UserName",
+              )),
+              TextFormField(decoration: const InputDecoration(
+                icon: Icon(Icons.password),
+                labelText: "Password",
+              ),
+              obscureText: true),
+              const SizedBox(height: 24),
+              ElevatedButton(onPressed: onPressed, child: Text("Let's go"))
+            ],
+        )
       )
-    );
+    ));
   }
 }
